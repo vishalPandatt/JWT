41c7f1b85178c49a1111b702ab4bbd1cd78362de
Vishal Vashishth <vishal112vp@gmail.com>
2026-09-23 23:00:36 +0530

    install some dependencies

--- jwt-react/src/App.js
+++ jwt-react/src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./pages/auth/login/Login";
 import Signup from "./pages/auth/signup/Signup";
 import Dashboard from "./pages/Dashboard/Dashboard";
@@ -6,6 +7,8 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
--- jwt-react/src/configration/dbconfig.js
+++ jwt-react/src/configration/dbconfig.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/27017/jwt");
+mongoose.connect("mongodb://127.0.0.1:27017/jwt");
 
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB");
